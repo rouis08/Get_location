0b095209a819ecbc0ffc5adfc86b4fa508edfdf6
LouisLee <rouis08@yahoo.com.tw>
2022-04-09 01:10:19 +0800

    get location only once without updated behavior there is a timer function there is a delay function

--- Get_location/ViewController.swift
+++ Get_location/ViewController.swift
@@ -5,15 +5,52 @@
 //  Created by Shao Hua Lee on 4/9/22.
 //
 
+
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var locationManager:CLLocationManager!
+
     override func viewDidLoad() {
-        super.viewDidLoad()
+
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        let coordinate = locationManager.location?.coordinate
+        print("latitude: \(String(describing: coordinate?.latitude))")
+        print("longtude: \(String(describing: coordinate?.longitude))")
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            print("latitude: \(String(describing: coordinate?.latitude))")
+            print("longtude: \(String(describing: coordinate?.longitude))")
+        }
+    
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+            self.ticker()
+            
+        }
+    }
+
+    func ticker() {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "hh:mm:ss"
+            let whatTime = formatter.string(from: Date())
+            
+            
+            print(whatTime)
+            let coordinate = locationManager.location?.coordinate
+            print("latitude: timer \(String(describing: coordinate?.latitude))")
+            print("longtude: timer \(String(describing: coordinate?.longitude))")
+        }
+
+        
+
     }
 
 
-}
 
