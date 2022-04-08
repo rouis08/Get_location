f49c426b645b91b56db2608a3b31532a4d5047d7
LouisLee <rouis08@yahoo.com.tw>
2022-04-09 00:15:34 +0800

    Initial Commit

---
+++ Get_location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get_location
+//
+//  Created by Shao Hua Lee on 4/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
